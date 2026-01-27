6883ab7d5b624b7d50de6f26ae9bd19728df3d88
unknown <Shaimaaabuhamed1672002@gmail.com>
2026-01-27 17:26:51 +0200

    commit

---
+++ lib/controller/getx/controllers/donation_history/donation_history_controller.dart
@@ -0,0 +1,50 @@
+import 'package:crowdfunding_platform/controller/core/constants/images_manager.dart';
+import 'package:crowdfunding_platform/model/donation_summary.dart';
+import 'package:get/get.dart';
+
+class DonationHistoryController extends GetxController {
+  // قائمة التبرعات باستخدام المودل الخاص بك
+  var donations = <DonationSummary>[].obs;
+  String get streamlineImage => Get.isDarkMode
+      ? ImagesManager.streamlineDark
+      : ImagesManager.streamlineLight;
+
+  @override
+  void onInit() {
+    super.onInit();
+    // تجربة: فك التعليق عن السطر بالأسفل لتجربة الحالة المليانة
+    //_loadMockData();
+  }
+
+  String formatTimeAgo(DateTime dateTime) {
+    final duration = DateTime.now().difference(dateTime);
+
+    if (duration.inDays >= 30) {
+      return "منذ ${(duration.inDays / 30).floor()} شهر";
+    } else if (duration.inDays >= 7) {
+      return "منذ ${(duration.inDays / 7).floor()} أسبوع";
+    } else if (duration.inDays >= 1) {
+      return duration.inDays == 1 ? "أمس" : "منذ ${duration.inDays} يوم";
+    } else if (duration.inHours >= 1) {
+      return "منذ ${duration.inHours} ساعة";
+    } else if (duration.inMinutes >= 1) {
+      return "منذ ${duration.inMinutes} دقيقة";
+    } else {
+      return "الآن";
+    }
+  }
+
+  void _loadMockData() {
+    donations.assignAll([
+      DonationSummary(
+        title: "قطرة حياة : مياه نظيفة لأطفال غزة",
+        amount: 50,
+        currency: "شيكل",
+        campaignImage: ImagesManager.campaignReview,
+        numberStars: 50,
+        donationDate: DateTime(2025, 7, 16),
+      ),
+      // أضف المزيد هنا...
+    ]);
+  }
+}
