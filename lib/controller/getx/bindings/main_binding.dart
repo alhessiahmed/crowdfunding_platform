d622d811a7e478f8ef8073db0226c2675015ccfa
alhessiahmed <alhessiahmed@gmail.com>
2026-01-27 16:46:31 +0200

    trying to fix merge conflict with malak branch

--- lib/controller/getx/bindings/main_binding.dart
+++ lib/controller/getx/bindings/main_binding.dart
@@ -1,16 +1,14 @@
-import 'package:crowdfunding_platform/controller/getx/bindings/my_campagins_binding.dart';
 import 'package:crowdfunding_platform/controller/getx/controllers/discover_controller.dart';
 import 'package:crowdfunding_platform/controller/getx/controllers/my_campagins_controller.dart';
 import 'package:crowdfunding_platform/controller/getx/controllers/profile_controller.dart';
 import 'package:get/instance_manager.dart';
 
-class MainBinding implements Bindings{
+class MainBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(()=>DiscoverController());
-    Get.lazyPut(()=>MyCampaginsController());
-    Get.lazyPut(()=>ProfileController());
-        Get.lazyPut(()=> DiscoverController());
-
+    Get.lazyPut(() => DiscoverController());
+    Get.lazyPut(() => MyCampaginsController());
+    Get.lazyPut(() => ProfileController());
+    Get.lazyPut(() => DiscoverController());
   }
 }
