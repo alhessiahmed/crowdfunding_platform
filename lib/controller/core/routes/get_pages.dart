435be22c8562a0a21ac506a6fa557af509e699e6
Fawzi Mohammmed <fawmoh2014@gmail.com>
2026-02-03 21:26:05 +0200

    start RequestToWithdrawProfitsScreen

--- lib/controller/core/routes/get_pages.dart
+++ lib/controller/core/routes/get_pages.dart
@@ -7,6 +7,7 @@ import 'package:crowdfunding_platform/controller/getx/bindings/control_campaign_
 import 'package:crowdfunding_platform/controller/getx/bindings/edit_campaign_details_binding.dart';
 import 'package:crowdfunding_platform/controller/getx/bindings/home_binding.dart';
 import 'package:crowdfunding_platform/controller/getx/bindings/donar_home_binding.dart';
+import 'package:crowdfunding_platform/controller/getx/bindings/request_to_withdraw_profits_binding.dart';
 import 'package:crowdfunding_platform/controller/getx/bindings/wallet_binding.dart';
 import 'package:crowdfunding_platform/view/screens/add_pymaent_method/add_payment_method_page.dart';
 import 'package:crowdfunding_platform/view/screens/add_pymaent_method/choose_payment_method_page.dart';
@@ -23,6 +24,7 @@ import 'package:crowdfunding_platform/view/screens/main_screen.dart';
 import 'package:crowdfunding_platform/view/screens/update_campaign/add_update_to_campagin_screen.dart';
 import 'package:crowdfunding_platform/view/screens/update_campaign/control_campaign_screen.dart';
 import 'package:crowdfunding_platform/view/screens/update_campaign/edit_campaign_details_screen.dart';
+import 'package:crowdfunding_platform/view/screens/wallet/Request_to_withdraw_profits_screen.dart';
 import 'package:crowdfunding_platform/view/screens/wallet/wallet_screen.dart';
 import 'package:get/get.dart';
 
@@ -181,6 +183,11 @@ final List<GetPage<dynamic>> getPages = [
     name: RoutesManager.walletScreen,
     page: () => const WalletScreen(),
     binding: WalletBinding(),
+  ),
+  GetPage(
+    name: RoutesManager.requestToWithdrawProfitsScreen,
+    page: () => const RequestToWithdrawProfitsScreen(),
+    binding: RequestToWithdrawProfitsBinding(),
   ),
 
   // GetPage(
---
+++ lib/controller/getx/controllers/wallet/request_to_withdraw_profits_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class RequestToWithdrawProfitsController extends GetxController {
+  final int totalStares = 5000;
+  final int mony = 50000;
+}
