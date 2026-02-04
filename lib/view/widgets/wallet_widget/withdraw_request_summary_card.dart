bdf44ad0ad42882975b1af625c9f38d6e6e31e7b
Fawzi Mohammmed <fawmoh2014@gmail.com>
2026-02-04 19:19:41 +0200

    done Request_to_withdraw_profits_screen.dart

---
+++ lib/view/widgets/wallet_widget/withdraw_request_summary_card.dart
@@ -0,0 +1,67 @@
+import 'package:crowdfunding_platform/controller/core/constants/colors_manager.dart';
+import 'package:crowdfunding_platform/view/widgets/update_campaign_widget/app_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart' as intl;
+
+class WithdrawRequestSummaryCard extends StatelessWidget {
+  const WithdrawRequestSummaryCard({
+    super.key,
+    required this.listOfSummaryItems,
+  });
+  final List<Widget> listOfSummaryItems;
+  @override
+  Widget build(BuildContext context) {
+    return AppCard(
+      borderRadius: 32.r,
+      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 32.h),
+      child: Column(children: listOfSummaryItems),
+    );
+  }
+}
+
+class TextOfTheSummary extends StatelessWidget {
+  const TextOfTheSummary({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.thrillingText,
+  });
+  final String title;
+  final int value;
+  final String thrillingText;
+  @override
+  Widget build(BuildContext context) {
+    final numberFormatter = intl.NumberFormat('#,###');
+    final formattedValue = numberFormatter.format(value);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: Theme.of(
+            context,
+          ).textTheme.bodySmall?.copyWith(color: ColorsManager.secondaryLight),
+        ),
+        Row(
+          children: [
+            Text(
+              ' $formattedValue',
+              textDirection: TextDirection.ltr,
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: ColorsManager.secondaryLight,
+              ),
+            ),
+            Text(
+              thrillingText,
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: ColorsManager.secondaryLight,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
